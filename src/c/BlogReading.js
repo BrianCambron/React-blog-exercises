b7882ca9f91c5cc1475f0e1ac0096bd2e3dacd8d
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-30 21:33:52 -0400

    completed c

--- src/c/BlogReading.js
+++ src/c/BlogReading.js
@@ -9,15 +9,21 @@ class BlogReading extends Component {
     super(props);
     this.state = {
       blogs: [{title: 'Blog1', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},{title:'Blog2', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},{title:'Blog3', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}],
+      pickedBlog: {},
     }
-    // this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
-  handleClick(blogs){
-  console.log(blogs);
-   };
+  // handleClick(blogs){
+  // console.log(blogs);
+  //  };
+  handleClick(blogIndex) {
+      const blogPost = this.state.blogs[blogIndex];
+      this.setState({pickedBlog: blogPost});
+      console.log(this.state.pickedBlog);
+    }
 
-  render(){
-    const blogs = this.state.blogs.map((blog, index) => { let item = this.handleClick.bind(this, blog); return <li onClick={item} key={index}><h5>{blog.title}</h5><p className="blogBody">{blog.body}</p></li>});
+  render() {
+    const blogs = this.state.blogs.map((blog, index) => (<li onClick={() => this.handleClick(index)} key={index}><h5>{blog.title}</h5><div className="blogBody"><p>{blog.body}</p></div></li>));
     return(
       <React.Fragment>
         <div className="left-aside col-12 col-md-4">
@@ -27,23 +33,21 @@ class BlogReading extends Component {
           </ul>
         </div>
       </div>
-      <BlogRight />
+      <BlogRight pickedBlog={this.state.pickedBlog}/>
       </React.Fragment>
     )
   }
 }
 
 class BlogRight extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.setState = {
-  //   bloginview: [{}]
-  //   }
-  // }
   render(){
-    console.log(this.props.blogs);
+    console.log(this.props.pickedBlog);
     return(
+      <div className="col-12 col-md-4 clicked-blog">
       <div>
+      <h5>{this.props.pickedBlog.title}</h5>
+      <p>{this.props.pickedBlog.body}</p>
+      </div>
       </div>
     )
   }
